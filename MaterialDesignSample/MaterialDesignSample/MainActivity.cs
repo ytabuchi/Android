77b5ad805920d4db4097fbc9c22f7bd355929eac
Yoshito Tabuchi <ytabuchi@xlsoft.com>
2015-04-30 00:03:22 +0900

    MaterialDesignSample w/o AppCompat v7

---
+++ MaterialDesignSample/MaterialDesignSample/MainActivity.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace MaterialDesignSample
+{
+    [Activity(Label = "MaterialDesignSample", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle bundle) {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button>(Resource.Id.myButton);
+			
+            button.Click += delegate {
+                button.Text = string.Format("{0} clicks!", count++);
+            };
+        }
+
+        // Get OptionMenu resorce from /menu/main_menu.xml
+        public override bool OnCreateOptionsMenu(IMenu menu) {
+            MenuInflater.Inflate(Resource.Menu.main_menu, menu);
+            return base.OnCreateOptionsMenu(menu);
+        }
+        public override bool OnOptionsItemSelected(IMenuItem item) {
+            Toast.MakeText(this, "Tapped: " + item.TitleFormatted, ToastLength.Short).Show();
+            return base.OnOptionsItemSelected(item);
+        }
+    }
+}
+
+

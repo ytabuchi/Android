cac5ef621885d828a81ee815a76b85ac5ce72991
Yoshito Tabuchi @ MBP-Xamarin <ytabuchi.xlsoft@gmail.com>
2017-11-09 18:29:49 +0900

    add TabLayoutSample and add DesignLibrary

--- TabLayoutSample/TabLayoutTab/MainActivity.cs
+++ TabLayoutSample/TabLayoutTab/MainActivity.cs
@@ -1,11 +1,13 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Support.V7.App;
+using Android.Support.Design.Widget;
 
 namespace TabLayoutTab
 {
     [Activity(Label = "TabLayoutTab", MainLauncher = true)]
-    public class MainActivity : Activity
+    public class MainActivity : AppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -13,6 +15,10 @@ namespace TabLayoutTab
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+
+            var tabLayout = FindViewById<TabLayout>(Resource.Id.tabContainer);
+            tabLayout.AddTab(tabLayout.NewTab().SetText("Tab1"));
+            tabLayout.AddTab(tabLayout.NewTab().SetText("Tab2"));
         }
     }
 }
